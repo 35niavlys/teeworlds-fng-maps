ee2cd5c8ff19d4d129991e2d6b059a4cd3185b73
35niavlys <35niavlys@gmail.com>
2019-04-01 18:55:29 +0000

    avoid current map + code cleaning

--- run.sh
+++ run.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-cd ${0%/*}
+cd "${0%/*}"
 
 RANDOM_MAP_FILE="conf/vote_random_map"
 
 function update_random_map {
+	CURRENT="$1"
+	echo "### updating random maps. Current is $CURRENT"
 	echo "### updating ${RANDOM_MAP_FILE}.cfg"
 	unset LAST
 	LAST=$(cat ${RANDOM_MAP_FILE}.cfg 2>/dev/null)
-	find data/maps/ -type f -name '*.map' | grep -v "${LAST:-\0}" | sort -R | head -1 | sed 's/^data\/maps\//sv_map /' | sed 's/.map$//' > "${RANDOM_MAP_FILE}.cfg"
+	find data/maps/ -type f -name '*.map' | grep -v "$CURRENT" | grep -v "${LAST:-\0}" | sort -R | head -1 | sed 's/^data\/maps\//sv_map /' | sed 's/.map$//' > "${RANDOM_MAP_FILE}.cfg"
 	echo -e "\t-$(cat ${RANDOM_MAP_FILE}.cfg)"
 	for I in {0..5} ; do
 		echo "### updating ${RANDOM_MAP_FILE}_${I}.cfg"
@@ -19,14 +21,14 @@ function update_random_map {
 	done
 }
 
-./openfng.sh $@ | while read LINE ; do
+./openfng.sh "$@" | while read -r LINE ; do
 	echo "[$(date '+%d/%m/%Y %H:%M:%S')] $LINE"
 	if grep -q "\[datafile\]: loading done" <<<"$LINE" ; then
-		update_random_map
-	elif grep "\[game\]: activate_random_rotation_" <<<"$LINE" | grep -qv "\[say\]" ; then
-		RANK=$(sed 's/.*activate_random_rotation_//' <<<"$LINE")
-		cat ${RANDOM_MAP_FILE}_${RANK}.cfg | ./sendToServer.sh
+		LAST=$(sed "s#.*datafile='maps/\(.*\)\.map'#\1#" <<<"$LINE")
+		update_random_map "$LAST"
 	elif grep "\[game\]: activate_random_rotation" <<<"$LINE" | grep -qv "\[say\]" ; then
-		cat ${RANDOM_MAP_FILE}.cfg | ./sendToServer.sh
+		RANK=${LINE//*activate_random_rotation/}
+		./sendToServer.sh < "${RANDOM_MAP_FILE}${RANK}.cfg"
 	fi
 done |& tee -a ./logs/teeworlds.log
+
